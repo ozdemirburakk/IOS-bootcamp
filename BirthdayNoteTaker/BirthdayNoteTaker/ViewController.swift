3be6361cc0b3cd36223cca01a3ae8018f5c2ec36
Burak Ozdemir <ozdemirburak1912@gmail.com>
2025-10-09 14:40:26 +0300

    IOS06-BirthdayNoteTaker

---
+++ BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Burak Özdemir on 9.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            nameLabel.text = "Name: \(newName)"
+        }
+        
+        if let newBirthday = storedBirthday as? String {
+            birthdayLabel.text = "Birthday: \(newBirthday)"
+        }
+    }
+
+    @IBAction func saveClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
+        
+        nameLabel.text = "Name: " + nameTextField.text!
+        birthdayLabel.text = "Birthday: " + birthdayTextField.text!
+        
+    }
+    
+    
+    @IBAction func deleteClicked(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if  (storedName as? String) != nil{
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name: "
+        }
+        
+        if (storedBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "Birthday: "
+        }
+    }
+}
+
